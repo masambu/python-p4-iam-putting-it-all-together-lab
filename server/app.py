67ce157bf3c7768126507fbbc053dad60be048ef
Kelvin Masambu <kevohmasambu@gmail.com>
2024-04-23 14:59:21 +0300

    done

--- server/app.py
+++ server/app.py
@@ -1,14 +1,39 @@
 #!/usr/bin/env python3
 
-from flask import request, session
+from flask import request, session, jsonify
 from flask_restful import Resource
+from werkzeug.security import generate_password_hash
 from sqlalchemy.exc import IntegrityError
 
 from config import app, db, api
 from models import User, Recipe
 
 class Signup(Resource):
-    pass
+    def post(self):
+        data= request.get_json()
+
+        username = data.get('username')
+        password=data.get('password')
+        image_url = data.get('image_url')
+        bio = data.get('bio')
+
+        encrypted_password = generate_password_hash(password)
+
+        new_user = User(username=username, password= encrypted_password,image_url=image_url, bio=bio)
+
+        db.sesion.add(new_user)
+        db.session.commit()
+
+        session['user_id'] = new_user.id
+
+        response_data = {
+            'id': new_user.id,
+            'username':new_user.username,
+            'image_url': new_user.image_url,
+            'bio': new_user.bio
+        }
+
+        return jsonify(response_data), 201
 
 class CheckSession(Resource):
     pass
